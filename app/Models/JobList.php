b77493255ff57ca847eadc33e9f2b80e99551d4e
unknown <zidane.nur004@gmail.com>
2024-12-23 22:48:24 +0700

    integration with mysql

---
+++ app/Models/JobList.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class JobList extends Model
+{
+    protected $fillable = ["name", "pay"];
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Support\Arr;
+use App\Models\JobList;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,36 +8,13 @@ Route::get('/', function () {
 });
 
 Route::get('/jobs', function () {
-    $jobs = [
-        [
-            "id" => 1,
-            "name" => "Programmer",
-            "pay" => "10.000",
-        ],
-        [
-            "id" => 2,
-            "name" => "Lecturer",
-            "pay" => "40.000"
-        ]
-    ];
+    $jobs = JobList::all();
     return view('jobs', ["jobs" => $jobs]);
 });
 
 Route::get('/job/{id}', function ($id) {
-    $jobs = [
-        [
-            "id" => 1,
-            "name" => "Programmer",
-            "pay" => "10.000",
-        ],
-        [
-            "id" => 2,
-            "name" => "Lecturer",
-            "pay" => "40.000"
-        ]
-    ];
 
-    $job = Arr::first($jobs, fn ($jobs) => $jobs["id"] == $id);
+    $job = JobList::find($id);
     
     return view('job', ["job" => $job]);
 });
